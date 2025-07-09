9ce906b6356bb51b13f3551d98631004508dfb78
LukaszzM <lukasz.malucha1@gmail.com>
2025-07-09 11:49:20 +0200

    feat: update error page

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,18 +1,22 @@
 "use client";
 
-import { ServerCrash } from "lucide-react";
-
-import { Container } from "@/components/ui/container";
+import { Logo } from "@/components/ui/logo";
 
 export default function GlobalError() {
   return (
     <html lang="en">
-      <body className="bg-popover sm:bg-background flex min-h-screen items-center justify-center text-center">
-        <Container className="items-center">
-          <ServerCrash size={44} />
-          <h1 className="text-2xl font-semibold">Internal Server Error</h1>
-          <p className="text-muted-foreground">Sorry, something went wrong on our servers. Please try again later.</p>
-        </Container>
+      <body className="bg-popover sm:bg-background flex min-h-screen items-center justify-center">
+        <main className="">
+          <div className="flex max-w-sm flex-col gap-4 p-4 sm:flex-row">
+            <Logo />
+            <div>
+              <h1 className="text-xl font-bold">Something went wrong</h1>
+              <p className="text-muted-foreground">
+                Sorry, we couldn&apos;t process your request at this time. Please try again later.
+              </p>
+            </div>
+          </div>
+        </main>
       </body>
     </html>
   );
